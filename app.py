6ceacfcb5e6f70658b29843759c6aa724249c1df
Mazin Hassan <mazinmhassan@gmail.com>
2024-12-17 22:46:55 -0500

    debugging multiple

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-import pandas as pd
-import streamlit as st
 import scipy.stats
-
-st.header('Tossing a Coin')
-
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
-start_button = st.button('Run')
-
-if start_button:
-    st.write(f'Running the experiment of {number_of_trials} trials.')
-
-st.write('It is not a functional application yet. Under construction.')
+import streamlit as st
+import time
 
 st.header('Tossing a Coin')
 
